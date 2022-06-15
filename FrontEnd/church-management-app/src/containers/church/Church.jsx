dd12732221a97202c4c41c5367b31dcd61e08109
samueljazzjohn <samueljazzjohn@gmail.com>
2022-06-16 00:10:56 +0530

    church dashboard design created

--- FrontEnd/church-management-app/src/containers/church/Church.jsx
+++ FrontEnd/church-management-app/src/containers/church/Church.jsx
@@ -1,10 +1,58 @@
 import React from 'react'
 import './church.css'
+import Dashboard from './Dashboard'
+import {Routes,Route} from 'react-router-dom'
+import { DashboardTab } from '../../components'
+import { AiOutlineHome } from 'react-icons/ai'
+import { GoDiffAdded } from 'react-icons/go'
+import { BiAlarmAdd, BiChurch } from 'react-icons/bi'
+import { MdOutlineManageAccounts } from 'react-icons/md'
+import {DashboardHometab} from '../../components'
 
 const Church = () => {
+
+  const menuItems = [{
+    icon: AiOutlineHome,
+    item: 'Dashboard',
+    url:'/church/'
+  },
+  {
+    icon: GoDiffAdded,
+    item: 'Add members',
+    url:'/church/add-members'
+  },
+  {
+    icon: BiAlarmAdd,
+    item: 'Add revenue expenditure',
+    url:'/church/add-revenue-expenditure'
+  },
+  {
+    icon: MdOutlineManageAccounts,
+    item: 'Add committee members',
+    url:'/admin/add-committee-members'
+  },
+  ]
   return (
     <div className="pgm__church_dashboard_container">
-      Church
+      <div className="pgm__church_dashboard_menu">
+        <div className="pgm__church_dashboard_header">
+          <h1 className="gradient__text">
+            Church
+          </h1>
+        </div>
+        <div className="pgm__church_dashboard_menu_links">
+          <DashboardTab items={menuItems} />
+        </div>
+      </div>
+      <div className="pgm__church_dashboard_content">
+        <div className="pgm__church_dashboard_hometab">
+          <DashboardHometab />
+        </div>
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          
+        </Routes>
+      </div>
     </div>
   )
 }
---
+++ FrontEnd/church-management-app/src/containers/church/Dashboard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+
+const Dashboard = () => {
+  return (
+    <div className="pgm__admin_dashboard">
+    </div>
+  )
+}
+
+export default Dashboard
