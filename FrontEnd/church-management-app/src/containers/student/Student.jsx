639460195d8b4754bb9d1a3f0b030e8670487dab
samueljazzjohn <samueljazzjohn@gmail.com>
2022-06-16 00:11:37 +0530

    student dashboard added

--- FrontEnd/church-management-app/src/containers/student/Student.jsx
+++ FrontEnd/church-management-app/src/containers/student/Student.jsx
@@ -1,10 +1,54 @@
 import React from 'react'
 import './student.css'
+import Dashboard from './Dashboard'
+import {Routes,Route} from 'react-router-dom'
+import { DashboardTab } from '../../components'
+import { AiOutlineHome } from 'react-icons/ai'
+import { GoDiffAdded } from 'react-icons/go'
+import { BiAlarmAdd } from 'react-icons/bi'
+import { MdOutlineManageAccounts } from 'react-icons/md'
+import {DashboardHometab} from '../../components'
 
 const Student = () => {
+
+  const menuItems = [{
+    icon: AiOutlineHome,
+    item: 'Dashboard',
+    url:'/student/'
+  },
+  {
+    icon: GoDiffAdded,
+    item: 'Complete works',
+    url:'/student/submit-works'
+  },
+  {
+    icon: BiAlarmAdd,
+    item: 'View notification',
+    url:'/student/view notification'
+  },
+  ]
+
   return (
     <div className="pgm__student_dashboard_container">
-      Student
+      <div className="pgm__student_dashboard_menu">
+        <div className="pgm__student_dashboard_header">
+          <h1 className="gradient__text">
+            Student
+          </h1>
+        </div>
+        <div className="pgm__student_dashboard_menu_links">
+          <DashboardTab items={menuItems} />
+        </div>
+      </div>
+      <div className="pgm__student_dashboard_content">
+        <div className="pgm__student_dashboard_hometab">
+          <DashboardHometab />
+        </div>
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          
+        </Routes>
+      </div>
     </div>
   )
 }
