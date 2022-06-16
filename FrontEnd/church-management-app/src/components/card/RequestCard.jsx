e06113471b129232d46ed518dea2f303db02acf4
samueljazzjohn <samueljazzjohn@gmail.com>
2022-06-16 23:41:49 +0530

    card component added

---
+++ FrontEnd/church-management-app/src/components/card/RequestCard.jsx
@@ -0,0 +1,25 @@
+import React,{useState} from 'react'
+import'./card.css'
+import {Card} from 'react-bootstrap'
+import { useNavigate } from 'react-router-dom'
+
+const RequestCard = (props) => {
+
+    const navigate=useNavigate()
+
+    const handleClick=()=>{
+        console.log('clicked')
+        navigate(props.url)
+    }
+
+  return (
+    <div className="pgm__request_card_container">
+        <Card className="pgm__request_card" body onClick={()=>handleClick()}>
+            <center><img src={props.image} alt="image" /></center> 
+            <p> {props.name}</p>
+        </Card>
+    </div>
+  )
+}
+
+export default RequestCard
