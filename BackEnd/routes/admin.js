c56247a6b439ab83612b04bc186c987cb6717944
samueljazzjohn <samueljazzjohn@gmail.com>
2022-06-19 23:12:15 +0530

    new end point created

--- BackEnd/routes/admin.js
+++ BackEnd/routes/admin.js
@@ -7,7 +7,8 @@ const UserModel = require('../models/userModel')
 const ChurchModel = require('../models/churchModel')
 const StudentModel = require('../models/studentModel')
 const TeacherModel = require('../models/teacherModel')
-const nodemailer = require('nodemailer')
+const nodemailer = require('nodemailer');
+const mailSendHelper = require('../helpers/mailSendHelper');
 require('dotenv').config()
 
 let transport = nodemailer.createTransport({
@@ -21,127 +22,125 @@ let transport = nodemailer.createTransport({
 });
 
 /* POST Course details. */
-router.post('/add-course', async(req, res, next)=> {
+router.post('/add-course', async (req, res, next) => {
   console.log(req.body)
 
-  await CourseModel.create(req.body).then(()=>{
-    res.status(200).json({'Message':'success'})
-  }).catch((err)=>{
-    res.status(401).json({"Message":err})
+  await CourseModel.create(req.body).then(() => {
+    res.status(200).json({ 'Message': 'success' })
+  }).catch((err) => {
+    res.status(401).json({ "Message": err })
   })
 });
 
-router.delete('/remove-course',async(req,res,next)=>{
+router.delete('/remove-course', async (req, res, next) => {
   console.log(req.query)
-  await CourseModel.deleteOne({_id:req.query.id}).catch((err)=>{
-    res.status(401).json({"Message":err})
+  await CourseModel.deleteOne({ _id: req.query.id }).catch((err) => {
+    res.status(401).json({ "Message": err })
   })
-  res.status(201).json({"Message":"success"})
+  res.status(201).json({ "Message": "success" })
 })
 
-router.post('/add-event', async(req, res, next)=> {
+router.post('/add-event', async (req, res, next) => {
   console.log(req.body)
 
-  await EventModel.create(req.body).then(()=>{
-    res.status(200).json({'Message':'success'})
-  }).catch((err)=>{
-    res.status(401).json({"Message":err})
+  await EventModel.create(req.body).then(() => {
+    res.status(200).json({ 'Message': 'success' })
+  }).catch((err) => {
+    res.status(401).json({ "Message": err })
   })
 });
 
 // get all event details
-router.get('/event',async(req,res,next)=>{
-  data=await EventModel.find()
-  if(!data){
-    return res.status(401).json({"Message":failed})
+router.get('/event', async (req, res, next) => {
+  data = await EventModel.find()
+  if (!data) {
+    return res.status(401).json({ "Message": failed })
   }
   return res.status(200).json(data)
 })
 
-router.delete('/remove-event',async(req,res,next)=>{
+router.delete('/remove-event', async (req, res, next) => {
   console.log(req.query)
-  const data=await EventModel.deleteOne({_id:req.query.id})
+  const data = await EventModel.deleteOne({ _id: req.query.id })
   console.log(data.deletedCount)
-  if(data.deletedCount==0){
-    return res.status(401).json({"Message":"failed"})
+  if (data.deletedCount == 0) {
+    return res.status(401).json({ "Message": "failed" })
   }
-  res.status(201).json({"Message":"success"})
+  res.status(201).json({ "Message": "success" })
 })
 
 // Get church user details
-router.get('/church-details',async(req,res,next)=>{
-  data=await UserModel.find({type:"church",status:"Pending"})
+router.get('/church-details', async (req, res, next) => {
+  data = await UserModel.find({ type: "church", status: "Pending" })
   console.log(data)
-  if(!data){
-    return res.status(401).json({'Message':'failed'})
+  if (!data) {
+    return res.status(401).json({ 'Message': 'failed' })
   }
   res.status(201).json(data)
-  
+
 })
 
 // Get student user details
-router.get('/student-details',async(req,res,next)=>{
-  data=await UserModel.find({type:"student",status:"Pending"})
+router.get('/student-details', async (req, res, next) => {
+  data = await UserModel.find({ type: "student", status: "Pending" })
   console.log(data)
-  if(!data){
-    return res.status(401).json({'Message':'failed'})
+  if (!data) {
+    return res.status(401).json({ 'Message': 'failed' })
   }
   res.status(201).json(data)
-  
+
 })
 
 // Get teacher user details
-router.get('/teacher-details',async(req,res,next)=>{
-  data=await UserModel.find({type:"teacher",status:"Pending"})
+router.get('/teacher-details', async (req, res, next) => {
+  data = await UserModel.find({ type: "teacher", status: "Pending" })
   console.log(data)
-  if(!data){
-    return res.status(401).json({'Message':'failed'})
+  if (!data) {
+    return res.status(401).json({ 'Message': 'failed' })
   }
   res.status(201).json(data)
-  
+
 })
 
-// Get church details
-router.get('/church-data',async(req,res,next)=>{
-  let body={type:req.query.type,email:req.query.email}
-  data=await ChurchModel.find({userId:mongoose.Types.ObjectId(req.query.id)})
-  if(!data){
-    return res.status(401).json({'Message':'failed'})
+// get details
+
+router.get('/details', async (req, res, next) => {
+  let body = { type: req.query.type, email: req.query.email }
+  if (req.query.type == 'church') {
+    console.log('church')
+    let data = await ChurchModel.find({ userId: mongoose.Types.ObjectId(req.query.id) })
+    if (!data) {
+      return res.status(401).json({ 'Message': 'failed' })
+    }
+    body = { ...body, data }
+    console.log(body)
+    return res.status(201).json(body)
+  } else if (req.query.type == 'student') {
+    console.log('student')
+    let data = await StudentModel.find({ userId: mongoose.Types.ObjectId(req.query.id) })
+    if (!data) {
+      return res.status(401).json({ 'Message': 'failed' })
+    }
+    console.log(data[0].courseId)
+    let course = await CourseModel.findOne({ _id: mongoose.Types.ObjectId(data[0].courseId) })
+    console.log("course"+course.courseName)
+    body = { ...body, data, course: course.courseName }
+    console.log(body)
+    return res.status(201).json(body)
+  } else {
+    console.log('teacher')
+    // let body = { type: req.query.type, email: req.query.email }
+    let data = await TeacherModel.find({ userId: mongoose.Types.ObjectId(req.query.id) })
+    if (!data) {
+      return res.status(401).json({ 'Message': 'failed' })
+    }
+    body = { ...body, data }
+    // console.log(body)
+    res.status(201).json(body)
   }
-  body={...body,data}
-  console.log(body)
-  res.status(201).json(body)
-  
 })
 
-// Get student details
-router.get('/student-data',async(req,res,next)=>{
-  let body={type:req.query.type,email:req.query.email}
-  data=await StudentModel.find({userId:mongoose.Types.ObjectId(req.query.id)})
-  if(!data){
-    return res.status(401).json({'Message':'failed'})
-  }
-  course=await CourseModel.find({_id:courseId})
-  body={...body,data,course:course.courseName}
-  console.log(body)
-  res.status(201).json(body)
-  
-})
-
-// Get teacher details
-router.get('/teacher-data',async(req,res,next)=>{
-  let body={type:req.query.type,email:req.query.email}
-  data=await TeacherModel.find({userId:mongoose.Types.ObjectId(req.query.id)})
-  if(!data){
-    return res.status(401).json({'Message':'failed'})
-  }
-  body={...body,data}
-  // console.log(body)
-  res.status(201).json(body)
-  
-})
-
-router.get('/interview-invite',async(req,res,next)=>{
+router.get('/interview-invite', async (req, res, next) => {
   const mailOptions = {
     from: 'sender@gmail.com', // Sender address
     to: req.query.email, // List of recipients
@@ -150,31 +149,63 @@ router.get('/interview-invite',async(req,res,next)=>{
           we are planning to conduct an interview via zoom platform we kindly request you to join in time
       zoom link:https://us02web.zoom.us/j/71364219724?pwd=QmtFU2V3UGFmcS9aZnRkcXViWklSUT09`, // Plain text body
   };
-  transport.sendMail(mailOptions, function(err, info) {
+  transport.sendMail(mailOptions, function (err, info) {
     if (err) {
       console.log(err)
-      res.status(401).json({"Error":err})
+      res.status(401).json({ "Error": err })
     } else {
-      res.status(201).json({"Message":"Mail send successfully"})
+      res.status(201).json({ "Message": "Mail send successfully" })
       // console.log(info);
     }
-});
+  });
 })
 
 
 // Accept request
-router.patch('/accept-request',async(req,res,next)=>{
-  console.log(req.query)
-  let doc=await UserModel.findOneAndUpdate({_id:mongoose.Types.ObjectId(req.query.id)},{status:'Approved'},{new:true})
-  if(!doc){
-    return res.status(401).json({"Message":"Failed"})
+router.patch('/accept-request', async (req, res, next) => {
+  console.log(req.body)
+  let doc = await UserModel.findOneAndUpdate({ _id: mongoose.Types.ObjectId(req.body.id) }, { status: 'Approved' }, { new: true })
+  if (!doc) {
+    return res.status(401).json({ "Message": "Failed" })
   }
-  res.status(200).json(doc)
+  mailSendHelper.acceptedMail(doc.email,doc.username,req.body.id)
+  return res.status(201).json({"Message":"Success"})
 })
 
 // Reject requests
-router.delete('/reject-request',async(req,res,next)=>{
-
+router.delete('/reject-request', async (req, res, next) => {
+  console.log(req.query)
+  if(req.query=='student'){
+    let delStudent=await StudentModel.deleteOne({userId:req.query.id})
+    if (delStudent.deletedCount == 0) {
+      return res.status(401).json({ "Message": "failed" })
+    }
+    let delUser=await UserModel.deleteOne({_id:req.query.id})
+    if (delUser.deletedCount == 0) {
+      return res.status(401).json({ "Message": "failed" })
+    }
+    return res.status(201).json({"Message":"Succes"})
+  }else if(req.query=='teacher'){
+    let delTeacher=await TeacherModel.deleteOne({userId:req.query.id})
+    if (delTeacher.deletedCount == 0) {
+      return res.status(401).json({ "Message": "failed" })
+    }
+    let delUser=await UserModel.deleteOne({_id:req.query.id})
+    if (delUser.deletedCount == 0) {
+      return res.status(401).json({ "Message": "failed" })
+    }
+    return res.status(201).json({"Message":"Succes"})
+  }else{
+    let delChurch=await ChurchModel.deleteOne({userId:req.query.id})
+    if (delChurch.deletedCount == 0) {
+      return res.status(401).json({ "Message": "failed" })
+    }
+    let delUser=await UserModel.deleteOne({_id:req.query.id})
+    if (delUser.deletedCount == 0) {
+      return res.status(401).json({ "Message": "failed" })
+    }
+    return res.status(201).json({"Message":"Succes"})
+  }
 })
 
 
