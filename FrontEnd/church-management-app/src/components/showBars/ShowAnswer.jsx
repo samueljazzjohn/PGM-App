48917dcb26b921b940b7c669961f17132ad86c32
samueljazzjohn <samueljazzjohn@gmail.com>
2022-06-28 13:32:45 +0530

    Added new show answer component

---
+++ FrontEnd/church-management-app/src/components/showBars/ShowAnswer.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import './showBars.css'
+import { Card, Button } from 'react-bootstrap'
+import { toast } from 'react-toastify'
+import axios from 'axios'
+import { MdDeleteOutline } from 'react-icons/md'
+import { BiShow } from 'react-icons/bi'
+import { useDispatch } from 'react-redux/es/exports'
+import { fetchDetails } from '../../features/admin/detailsSlice'
+import { showModelOpen } from '../../features/showModel/ShowModelSlice'
+
+const ShowBar = (props) => {
+
+    const dispatch = useDispatch()
+
+
+    return (
+        <div className="pgm__show_answer_container">
+            <div className="pgm__show_answer_card">
+
+                <card className="pgm__show_card_body" body>{props.name}</card>
+                {props.submission && <p>{props.submission}</p>}
+            </div>
+            <Button className="pgm__show_answer_button"><a href={props.url}>View</a></Button>
+        </div>
+    )
+}
+
+export default ShowBar
